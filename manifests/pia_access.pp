438c3119f59cf12813158527f02cfbef1f149afa
Dan Iverson <dan.iverson@minneapolismn.gov>
2018-10-17 08:09:43 -0500

    Remove 24 hour limit

--- manifests/pia_access.pp
+++ manifests/pia_access.pp
@@ -7,11 +7,11 @@ class io_filebeat::pia_access (
   $pia_domain_list.each |$domain_name, $pia_domain_info| {
     filebeat::prospector {"${domain_name}-pia-access":
       paths             => [
-        "${pia_domain_info['ps_cfg_home_dir']}/webserv/${domain_name}/servers/PIA/logs/PIA_access.log",
+        "${pia_domain_info['ps_cfg_home_dir']}/webserv/${domain_name}/servers/PIA/logs/PIA_access.log*",
       ],
       doc_type          => 'access_log',
       input_type        => 'log',
-      ignore_older      => '24h',
+      # ignore_older      => '24h',
       fields_under_root => true,
       tail_files        => true,
       fields            => $fields,
