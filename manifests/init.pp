5d12d194046b3c380230a9d18b1bb777e652b1f3
Dan Iverson <dan@daniverson.com>
2018-12-20 13:13:25 -0600

    Filebeat 5 and 6 Support (#3)
    * Add APPSRV log file support
    
    * Change pia refs to appserv for APPSRV logs
    
    * Remove 24 hour limit
    
    * Keep files not managed by Puppet
    
    * Add support for the exclude_lines prospector option
    
    * Define exclude_lines variable
    
    * Fix variable reassignment error
    
    * Add missing comma in appserv.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,14 +6,15 @@ class io_filebeat (
   $pia_domain_list           = hiera_hash('pia_domain_list', undef),
   $appserver_domain_list     = hiera_hash('appserver_domain_list', undef),
   $prcs_domain_list          = hiera_hash('prcs_domain_list', undef),
-  $config_dir                = '/opt/filebeat/conf.d',
-  $major_version              = '5.4.0',
+  $major_version              = '6.4.2',
   $weblogic                  = false,
   $pia_access                = false,
-  $appserver                 = false,
-  $prcs                      = false,
+  $appserv                   = false,
+  $prcs_logs                 = false,
+
   $fields                    = undef,
   $output                    = undef,
+  $exclude_lines             = '[]',
 ) {
 
   # contain ::filebeat
@@ -25,6 +26,7 @@ class io_filebeat (
   case $::osfamily {
     'windows': {
       $fileowner       = $domain_user
+      $config_dir      = 'C:/Program Files/Filebeat/conf.d'
     }
     'AIX': {
       $fileowner       = $psft_install_user_name
@@ -34,6 +36,7 @@ class io_filebeat (
     }
     default: {
       $fileowner       = $psft_install_user_name
+      $config_dir      = '/opt/filebeat/conf.d'
     }
   }
 
@@ -41,6 +44,7 @@ class io_filebeat (
     config_dir     => $config_dir,
     major_version  => $major_version,
     outputs        => $output,
+    purge_conf_dir => false,
   }
 
   if ($weblogic) {
@@ -51,6 +55,10 @@ class io_filebeat (
   }
   if ($appserver) {
     contain ::io_filebeat::appserver
+  }
+
+  if ($appserv) {
+    contain ::io_filebeat::appserv
   }
 
 }
--- manifests/pia_access.pp
+++ manifests/pia_access.pp
@@ -2,19 +2,24 @@ class io_filebeat::pia_access (
   $ensure          = $io_filebeat::ensure,
   $pia_domain_list = $io_filebeat::pia_domain_list,
   $fields          = $io_filebeat::fields,
+  $exclude_lines   = $io_filebeat::exclude_lines,
 ) inherits io_filebeat {
+
+  # if ($exclude_lines = undef) { $exclude_lines = '[]' }
 
   $pia_domain_list.each |$domain_name, $pia_domain_info| {
     filebeat::prospector {"${domain_name}-pia-access":
       paths             => [
-        "${pia_domain_info['ps_cfg_home_dir']}/webserv/${domain_name}/servers/PIA/logs/PIA_access*.log",
+        "${pia_domain_info['ps_cfg_home_dir']}/webserv/${domain_name}/servers/PIA/logs/PIA_access.log*",
+
       ],
       doc_type          => 'access_log',
       input_type        => 'log',
-      ignore_older      => '24h',
+      # ignore_older      => '24h',
       fields_under_root => true,
       tail_files        => true,
       fields            => $fields,
+      exclude_lines     => $exclude_lines,
     }
   }
 
